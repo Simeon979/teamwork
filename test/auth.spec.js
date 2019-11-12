ed65e5eab8e8cac8dd0e290cfb10b7ec8810a47a
Simeon <adegbolasimeon@gmail.com>
2019-11-12 04:33:04 +0100

    chore(testing):add tests for user signin

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -8,15 +8,27 @@ const { query } = require('../db');
 const { expect } = chai;
 chai.use(chaiHttp);
 
+const complete = {
+  firstName: 'Rockfeller',
+  lastName: 'Davies',
+  email: 'abc@def.ghi',
+  password: 'abcdefghi',
+  gender: 'male',
+  jobRole: 'Manager',
+  department: 'Quality Assurance',
+  address: 'No 0, Nowhere Ave, Unknown.',
+};
+
+const incomplete = {
+  firstName: 'Rockfeller',
+  lastName: 'Davies',
+  email: 'abc@def.ghi',
+};
+
 describe('POST /auth/create-user', () => {
   beforeEach(async () => query('delete from employees'));
 
   it('should not create an employee with incomplete information', async () => {
-    const incomplete = {
-      firstName: 'Rockfeller',
-      lastName: 'Davies',
-      email: 'abc@def.ghi',
-    };
     try {
       const res = await chai.request(app)
         .post('/auth/create-user')
@@ -30,16 +42,6 @@ describe('POST /auth/create-user', () => {
   });
 
   it('should create an employee with complete information', async () => {
-    const complete = {
-      firstName: 'Rockfeller',
-      lastName: 'Davies',
-      email: 'abc@def.ghi',
-      password: 'abcdefghi',
-      gender: 'male',
-      jobRole: 'Manager',
-      department: 'Quality Assurance',
-      address: 'No 0, Nowhere Ave, Unknown.',
-    };
     try {
       const res = await chai.request(app)
         .post('/auth/create-user')
@@ -50,6 +52,47 @@ describe('POST /auth/create-user', () => {
       expect(res.body.data).to.have.property('message', 'User account successfully created');
       expect(res.body.data).to.have.property('token').that.is.a('string');
       expect(res.body.data).to.have.property('userId').that.is.a('number');
+    } catch (err) {
+      expect.fail(err);
+    }
+  });
+});
+
+describe('POST /auth/signin', () => {
+  before(async () => {
+    try {
+      await query('delete from employees');
+      await chai.request(app)
+        .post('/auth/create-user')
+        .send(complete);
+    } catch (err) {
+      expect.fail(err);
+    }
+  });
+
+  it('successfully signs user with correct credentials in', async () => {
+    try {
+      const res = await chai.request(app)
+        .post('/auth/signin')
+        .send({ email: complete.email, password: complete.password });
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.have.property('status', 'success');
+      expect(res.body).to.have.property('data');
+      expect(res.body.data).to.have.property('token').that.is.a('string');
+      expect(res.body.data).to.have.property('userId').that.is.a('number');
+    } catch (err) {
+      expect.fail(err);
+    }
+  });
+
+  it('fails to sign in user with incorrect credentials', async () => {
+    try {
+      const res = await chai.request(app)
+        .post('/auth/signin')
+        .send({ email: complete.email, password: `${complete.password}incorrect` });
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.have.property('status', 'error');
+      expect(res.body).to.have.property('error', 'incorrect email and password combination');
     } catch (err) {
       expect.fail(err);
     }
