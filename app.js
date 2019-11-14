cf5878ac2a429e991f145df737527534e148bf32
Simeon <adegbolasimeon@gmail.com>
2019-11-14 13:16:23 +0100

    feature(endpoint):set up gif upload route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const authRouter = require('./routes/auth');
+const gifsRouter = require('./routes/gifs');
 
 const app = express();
 
 app.use(express.json());
 app.use('/auth', authRouter);
+app.use('/gifs', gifsRouter);
 
 const PORT = process.env.PORT || 3000;
 
