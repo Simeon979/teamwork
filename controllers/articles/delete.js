e9000788f01fc1ee0adc9031260026c4b713353e
Simeon <adegbolasimeon@gmail.com>
2019-11-15 04:03:27 +0100

    feature(endpoint):implement article deletion route

---
+++ controllers/articles/delete.js
@@ -0,0 +1,33 @@
+const { query } = require('../../db');
+const makeErrorResponse = require('../../domain/makeErrorResponse');
+
+const deleteArticle = async (req, res, next) => {
+  const { employeeid } = req.currentUser;
+  const { articleId } = req.params;
+
+  const sql = `
+    DELETE FROM articles
+    WHERE article_id=$1 AND poster_id=$2
+    RETURNING *
+    `;
+
+  try {
+    const params = [articleId, employeeid];
+    const result = await query(sql, params);
+
+    if (result.rows.length !== 1) {
+      return makeErrorResponse(res, 404, 'article not found');
+    }
+
+    return res.json({
+      status: 'success',
+      data: {
+        message: 'Article successfully deleted',
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = deleteArticle;
--- routes/articles.js
+++ routes/articles.js
@@ -2,8 +2,10 @@ const articlesRouter = require('express').Router();
 
 const newArticle = require('../controllers/articles/create');
 const updateArticle = require('../controllers/articles/update');
+const deleteArticle = require('../controllers/articles/delete');
 
 articlesRouter.post('/', newArticle);
 articlesRouter.patch('/:articleId', updateArticle);
+articlesRouter.delete('/:articleId', deleteArticle);
 
 module.exports = articlesRouter;
