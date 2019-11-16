790b752facbfcdb6c72c1dca9ee0a5cc54dd8ab9
Simeon <adegbolasimeon@gmail.com>
2019-11-16 16:03:12 +0100

    feature(endpoint):implement get gif route

---
+++ controllers/gifs/read.js
@@ -0,0 +1,45 @@
+const { query } = require('../../db');
+const makeErrorResponse = require('../../domain/makeErrorResponse');
+
+const getGif = async (req, res, next) => {
+  const { gifId } = req.params;
+
+  const sql1 = `
+  SELECT created_on, title, image_url
+  FROM uploaded_gifs
+  WHERE gif_id=$1
+  `;
+
+  const sql2 = `
+  SELECT comment_id, comment, poster_id
+  FROM gif_comments
+  WHERE gif_id=$1
+  `;
+
+  try {
+    const gifResult = await query(sql1, [gifId]);
+    if (gifResult.rows.length !== 1) {
+      return makeErrorResponse(res, 404, 'gif not found');
+    }
+    const [foundGif] = gifResult.rows;
+    const commentResult = await query(sql2, [gifId]);
+    return res.json({
+      status: 'success',
+      data: {
+        id: gifId,
+        createdOn: foundGif.created_on,
+        title: foundGif.title,
+        url: foundGif.image_url,
+        comments: commentResult.rows.map((comment) => ({
+          commentId: comment.comment_id,
+          comment: comment.comment,
+          authorId: comment.poster_id,
+        })),
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = getGif;
--- routes/gifs.js
+++ routes/gifs.js
@@ -6,9 +6,11 @@ const upload = multer({ dest: 'uploads' });
 const createGifs = require('../controllers/gifs/create');
 const deleteGifs = require('../controllers/gifs/delete');
 const createComment = require('../controllers/gifs/comments/create');
+const getGif = require('../controllers/gifs/read');
 
 gifsRouter.post('/', upload.single('image'), createGifs);
 gifsRouter.delete('/:gifId', deleteGifs);
 gifsRouter.post('/:gifId/comment', createComment);
+gifsRouter.get('/:gifId', getGif);
 
 module.exports = gifsRouter;
