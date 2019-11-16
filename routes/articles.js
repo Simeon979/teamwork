64c3555a95f6acae89f6dc874a107d32c93dea5d
Simeon <adegbolasimeon@gmail.com>
2019-11-16 04:03:05 +0100

    bug(path):correct wrong path reference

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,7 @@ const articlesRouter = require('express').Router();
 const newArticle = require('../controllers/articles/create');
 const updateArticle = require('../controllers/articles/update');
 const deleteArticle = require('../controllers/articles/delete');
-const createComment = require('../controllers/comments/create');
+const createComment = require('../controllers/articles/comments/create');
 
 articlesRouter.post('/', newArticle);
 articlesRouter.patch('/:articleId', updateArticle);
