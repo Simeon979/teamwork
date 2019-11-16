00e1854e544fb237bb26d927f15b2d5cc6c775bb
Simeon <adegbolasimeon@gmail.com>
2019-11-16 04:02:09 +0100

    feature(endpoint):implement create gif comment route

---
+++ controllers/gifs/comments/create.js
@@ -0,0 +1,58 @@
+const { body, sanitizeBody, validationResult } = require('express-validator');
+
+const { query } = require('../../../db');
+const makeErrorResponse = require('../../../domain/makeErrorResponse');
+
+const createComment = [
+  body('comment', 'cannot be empty').isLength({ min: 1 }),
+  sanitizeBody('*').escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return makeErrorResponse(res, 422, 'please fill out every field');
+    }
+
+    const { employeeid } = req.currentUser;
+    const sql = `
+    WITH inserted AS (
+      INSERT INTO gif_comments (
+        comment,
+        gif_id,
+        poster_id
+      )   VALUES ($1, $2, $3)
+      RETURNING *
+    )
+    SELECT comment, inserted.created_on, title
+    FROM inserted JOIN uploaded_gifs ON inserted.gif_id=uploaded_gifs.gif_id
+    `;
+
+    try {
+      const params = [
+        req.body.comment,
+        req.params.gifId,
+        employeeid,
+      ];
+
+      const result = await query(sql, params);
+
+      if (result.rows.length !== 1) {
+        return makeErrorResponse(res, 404, 'gif not found');
+      }
+      const [savedComment] = result.rows;
+
+      return res.json({
+        status: 'success',
+        data: {
+          message: 'comment successfully created',
+          createdOn: savedComment.created_on,
+          comment: savedComment.comment,
+          gifTitle: savedComment.title,
+        },
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
+
+module.exports = createComment;
--- routes/gifs.js
+++ routes/gifs.js
@@ -5,8 +5,10 @@ const upload = multer({ dest: 'uploads' });
 
 const createGifs = require('../controllers/gifs/create');
 const deleteGifs = require('../controllers/gifs/delete');
+const createComment = require('../controllers/gifs/comments/create');
 
 gifsRouter.post('/', upload.single('image'), createGifs);
 gifsRouter.delete('/:gifId', deleteGifs);
+gifsRouter.post('/:gifId/comment', createComment);
 
 module.exports = gifsRouter;
