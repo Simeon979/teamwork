d7e0aa2ee90a0616792b4715fd9bb55691e2582a
Simeon <adegbolasimeon@gmail.com>
2019-11-17 03:54:45 +0100

    chore(project):configure app to use cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const v1 = require('./routes/v1');
 const authorize = require('./services/session');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/v1/auth', v1.authRouter);
 app.use('/v1/gifs', authorize, v1.gifsRouter);
