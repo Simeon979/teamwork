d094f3477a9d006930606b1867bf68202de27ff6
Simeon <adegbolasimeon@gmail.com>
2019-11-17 17:53:00 +0100

    bug(ci):fix travis build error

--- db/000005.sql
+++ db/000005.sql
@@ -1,5 +1,5 @@
 INSERT INTO employees
   (firstname, lastname, email, passwordhash, gender, jobrole, dept, employeeaddress)
 VALUES
-  ('Administrator', 'Superuser', 'admin@admin.net', '$2y$10$92.sL4PZX6mHukoG3GANGurbukHKt.pYyHW.7A66AcYYiwDUVpTsq', 'male', 'administrator', 'admin', 'Administrator ave, Superuser street, Complete. Control')
+  ('Administrator', 'Superuser', 'admin@admin.net', '$2b$10$CPRJBNC1DBhVeoHrlqM4suvXVRPtouHcX8rBasN207.9aYLnM/rTO', 'male', 'administrator', 'admin', 'Administrator ave, Superuser street, Complete. Control')
   ;
