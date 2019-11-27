55e73191bf99971154fffcd6d4cf80555c3a3e6d
Simeon <adegbolasimeon@gmail.com>
2019-11-27 18:39:09 +0100

    feature(endpoint):handle unknown route

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use('/api/v1/gifs', authorize, v1.gifsRouter);
 app.use('/api/v1/articles', authorize, v1.articlesRouter);
 app.use('/api/v1/feed', authorize, v1.feedRouter);
 
+app.use((req, res, next) => res.status(404).json({ status: 'error', error: 'the requested resource was not found'}))
+
 const PORT = process.env.PORT || 3100;
 
 const server = app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
